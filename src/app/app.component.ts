39bc7ca72d4b867858a091d442b7db0400d39514
peeoen <theerawat.pon@gmail.com>
2018-07-07 20:00:53 +0700

    update tslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component, ViewChild } from '@angular/core';
+import { OneSignal, OSNotificationPayload } from '@ionic-native/onesignal';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { App, MenuController, Nav, Platform } from 'ionic-angular';
+import { isCordovaAvailable } from '../common/is-cordova-available';
+import { oneSignalAppId, sender_id } from '../config';
 import { ComponentsListPage } from '../pages/components/list/components.list.page';
 import { GoogleMapsPage } from '../pages/google-maps/google-maps.page';
-
 import { HomePage } from '../pages/home/home.page';
 import { SlideBoxPage } from '../pages/slide-box/slide-box.page';
 import { WordpressListPage } from '../pages/wordpress/list/wordpress.list.page';
-import { OneSignal, OSNotificationPayload } from '@ionic-native/onesignal';
-import { isCordovaAvailable } from '../common/is-cordova-available';
-import { oneSignalAppId, sender_id } from '../config';
+
+
 @Component({
 	templateUrl: 'app.html'
 })
@@ -44,13 +44,13 @@ export class MyApp {
 	initializeApp() {
 		this.platform.ready().then(() => {
 			this.statusBar.styleDefault();
-			if (isCordovaAvailable()){
+			if (isCordovaAvailable()) {
 				this.oneSignal.startInit(oneSignalAppId, sender_id);
-				  this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
-				  this.oneSignal.handleNotificationReceived().subscribe(data => this.onPushReceived(data.payload));
-				  this.oneSignal.handleNotificationOpened().subscribe(data => this.onPushOpened(data.notification.payload));
-				  this.oneSignal.endInit();
-			  }
+				this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.Notification);
+				this.oneSignal.handleNotificationReceived().subscribe(data => this.onPushReceived(data.payload));
+				this.oneSignal.handleNotificationOpened().subscribe(data => this.onPushOpened(data.notification.payload));
+				this.oneSignal.endInit();
+			}
 		});
 	}
 
@@ -62,7 +62,7 @@ export class MyApp {
 	private onPushReceived(payload: OSNotificationPayload) {
 		alert('Push recevied:' + payload.body);
 	}
-	
+
 	private onPushOpened(payload: OSNotificationPayload) {
 		alert('Push opened: ' + payload.body);
 	}
