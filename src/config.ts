f5556983e7d4fd16e00fcf6d0712a97351a87897
Theerawat Burawat <theerawat.pon@gmail.com>
2018-07-06 20:36:06 -0700

    init commit

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Config {
+	public wordpressApiUrl = 'http://demo.titaniumtemplates.com/wordpress/?json=1';
+}
+export const sender_id = '568589664160';
+export const oneSignalAppId = '477b00c9-5d49-4a6f-b417-9877d63479ff';
